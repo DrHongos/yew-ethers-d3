54f545963733bb3b1a8a5ed7b893450b6c0afa0e
drhongos <lucasziegenfuhs@gmail.com>
2022-09-16 19:48:06 +0200

    Yew template, gets latest block and displays in a table

--- src/app.rs
+++ src/app.rs
@@ -1,12 +1,165 @@
+/////////////////////////////////////////////////////////////////
+//      WATCH BLOCKS AND REPORT DATA
+//      add links to explorer
+//      PLOT VARIATION OR SOMETHING INTERESTING
+//  https://github.com/gakonst/ethers-rs/blob/master/examples/subscribe_logs.rs
+//  https://github.com/gakonst/ethers-rs/blob/master/examples/watch_blocks.rs
+//  https://github.com/yewstack/yew/tree/master/examples   // NEW EXAMPLES!!
+/////////////////////////////////////////////////////////////////
 use yew::prelude::*;
+use ethers::{prelude::*, utils::format_units};
+use std::sync::Arc;
+use gloo_timers::callback::Interval;
 
-#[function_component(App)]
-pub fn app() -> Html {
-    html! {
-        <main>
-            <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-            <h1>{ "Hello World!" }</h1>
-            <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
-        </main>
+const API_MAINNET_KEY: &str = dotenv!("INFURA_WSS_KEY_MAINNET");
+
+pub enum AppMsg {
+    SetClient(Provider<Ws>),
+    FetchBlocks,
+    SetLastBlock(Block<H256>),    
+    SetError(String),
+    StartInterval,
+    StopInterval,
+}
+pub struct App {
+    client: Option<Arc<Provider<Ws>>>,
+    last_block: Option<Block<H256>>,
+    interval: Option<Interval>,
+    error: Option<String>,
+}
+
+impl Component for App {
+    type Message = AppMsg;
+    type Properties = ();
+    fn create(ctx: &Context<Self>) -> Self {
+        ctx.link().send_future(async {
+            match Provider::<Ws>::connect(API_MAINNET_KEY)
+            .await {
+                Ok(prov) => {
+                    AppMsg::SetClient(prov)
+                },
+                Err(err) => AppMsg::SetError(err.to_string())
+            }
+        });
+        Self {
+            client: None,
+            last_block: None,
+            error: None,
+            interval: None,
+        }
+    }
+
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {            
+            AppMsg::FetchBlocks => {
+                let client = Arc::clone(&self.client.as_ref().unwrap());
+                ctx.link().send_future(async move {
+                    match client.get_block(BlockNumber::Latest).await {
+                        Ok(block) => {
+                            let block = block.unwrap();
+                            AppMsg::SetLastBlock(block)
+                        },
+                        Err(err) => AppMsg::SetError(err.to_string())
+                    }
+                });
+                false
+            }
+            AppMsg::StartInterval => {
+                let handle = {
+                    let link = ctx.link().clone();
+                    Interval::new(12_000, move || link.send_message(AppMsg::FetchBlocks))
+                };
+                self.interval = Some(handle);
+                log::info!("Started interval");
+                true
+            }
+            AppMsg::StopInterval => {
+                //handler.cancel(); // HOW TO DEREFERENCE? IS THAT THE GWEI?
+                self.interval = None;                // this works!
+                log::info!("Stopped interval");
+                true
+            }
+            AppMsg::SetClient(provider) => {
+                self.client = Some(Arc::new(provider));
+                ctx.link().send_message(AppMsg::FetchBlocks);
+                true
+            }
+            AppMsg::SetLastBlock(bn) => {
+                self.last_block = Some(bn);
+                true
+            }
+            AppMsg::SetError(err) => {
+                self.error = Some(err);
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        html! {
+            <main>
+                <h1>{ "Hello Rust!" }</h1>
+                if !self.client.is_none() {
+                    if self.interval.is_none() {
+                        <button onclick={ctx.link().callback(|_| AppMsg::StartInterval)}>
+                            {"Start interval"}
+                        </button>
+                    } else {
+                        <button onclick={ctx.link().callback(|_| AppMsg::StopInterval)}>
+                            {"Stop interval"}
+                        </button>
+                    }
+                }
+                {self.table_block()}
+                if let Some(error) = &self.error {
+                    <p>{ format!("Error: {:?}", error) }</p>
+                }
+
+            </main>
+        }
+    }
+    
+}
+
+impl App {
+    pub fn table_block(&self) -> Html {
+        if let Some(last_block) = &self.last_block {
+            html! {
+                <table>
+                    <tr>
+                        <th>{"key"}</th>
+                        <th>{"value"}</th>
+                    </tr>
+                    <tr>
+                        <td>{"number"}</td> // make it a link
+                        <td>{last_block.number.unwrap()}</td>
+                    </tr>
+                    <tr>
+                        <td>{"hash"}</td>
+                        <td>{last_block.hash.unwrap()}</td>
+                    </tr>
+                    <tr>
+                        <td>{"basefee"}</td>
+                        <td>
+                        {format!("{:.3} gwei", format_units(last_block.base_fee_per_gas.unwrap(), 9).unwrap())}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>{"gas used"}</td>
+                        <td>{last_block.gas_used}</td>
+                    </tr>
+                    <tr>
+                        <td>{"tx.len()"}</td>
+                        <td>{last_block.transactions.len()}</td>
+                    </tr>
+                    <tr>
+                        <td>{"size"}</td>
+                        <td>{last_block.size.unwrap()} {" bytes"}</td>
+                    </tr>
+                </table>
+            }
+        } else {
+            html! {}
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+
+#[macro_use]
+extern crate dotenv_codegen;
+
 mod app;
+use wasm_logger;
 
 use app::App;
 
 fn main() {
+    wasm_logger::init(wasm_logger::Config::new(log::Level::Trace));
     yew::start_app::<App>();
 }
